55146821140c3b02b86070b129f63063a813c494
byBlair <jkas8233@naver.com>
2025-08-07 23:09:50 +0900

    feat: 초기세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+    return (
+      <div className="text-2xl font-bold text-blue-600">
+        2025 Portfolio with React + TypeScript + Tailwind!
+      </div>
+    )
+  }
+  
+  export default App
+  
